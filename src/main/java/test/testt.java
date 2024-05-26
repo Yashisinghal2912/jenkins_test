d4d29ba1e12f536fa890d13481419b302654d01f
Yashi Singhal <yashisinghal2923@gmail.com>
2024-05-26 11:17:26 +0530

    new_jenkins_file

---
+++ src/main/java/test/testt.java
@@ -0,0 +1,14 @@
+package test;
+
+public class testt {
+	int sum (int a,int b)
+	{
+		return a+b;
+	}
+	public static void main(String args[])
+	{
+		testt t=new testt();
+		System.out.println("Sum"+t.sum(20,40));
+	}
+
+}
